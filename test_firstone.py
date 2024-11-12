1eb8d473153dcb8392cc36f112d60d4e5ce9a12c
8886768315 <adibhupalreddy@gmail.com>
2024-11-12 10:52:06 +0100

    Update test_firstone.py

--- test_firstone.py
+++ test_firstone.py
@@ -1,3 +1,16 @@
 import pytest
+
 def test_my():
+    print("hello Jenkins")
     assert 1 + 1 == 2
+
+def test_my1():
+    print("hello Jenkins")
+    assert 1 + 1 == 2
+
+def test_my2():
+    print("hello Jenkins")
+    assert 1 + 1 == 2
+
+if __name__ == "__main__":
+    pytest.main()
